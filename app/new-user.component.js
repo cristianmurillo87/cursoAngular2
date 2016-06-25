d2edc1d57f667700bcce0b89b29c362a7b71dc13
cristianmurillo87 <cam_725@hotmail.com>
2016-06-25 18:20:32 -0500

    Formulario de agregar usuario configurado como model-driven form

--- app/new-user.component.js
+++ app/new-user.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', 'angular2/common'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,22 +10,52 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, common_1;
     var NewUserComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (common_1_1) {
+                common_1 = common_1_1;
             }],
         execute: function() {
             NewUserComponent = (function () {
-                function NewUserComponent() {
+                function NewUserComponent(fb) {
+                    this.form = fb.group({
+                        users: fb.group({
+                            username: ['', common_1.Validators.compose([
+                                    common_1.Validators.required
+                                ])],
+                            email: ['', common_1.Validators.compose([
+                                    common_1.Validators.required
+                                ])],
+                            phone: ['', common_1.Validators.compose([
+                                    common_1.Validators.required
+                                ])]
+                        }),
+                        address: fb.group({
+                            street: ['', common_1.Validators.compose([
+                                    common_1.Validators.required
+                                ])],
+                            suite: ['', common_1.Validators.compose([
+                                    common_1.Validators.required
+                                ])],
+                            city: ['', common_1.Validators.compose([
+                                    common_1.Validators.required
+                                ])],
+                            zipcode: ['', common_1.Validators.compose([
+                                    common_1.Validators.required
+                                ])]
+                        })
+                    });
                 }
                 NewUserComponent = __decorate([
                     core_1.Component({
                         templateUrl: 'app/new-user.component.html'
                     }), 
-                    __metadata('design:paramtypes', [])
+                    __metadata('design:paramtypes', [common_1.FormBuilder])
                 ], NewUserComponent);
                 return NewUserComponent;
             }());
--- app/new-user.component.ts
+++ app/new-user.component.ts
@@ -1,8 +1,41 @@
 import {Component} from 'angular2/core';
+import {ControlGroup, FormBuilder, Validators} from 'angular2/common';
 
 @Component({
     templateUrl:'app/new-user.component.html'
 })
 export class NewUserComponent{
-    
+    form: ControlGroup;
+
+    constructor(fb: FormBuilder){
+        this.form = fb.group({
+            users: fb.group({
+                username:['', Validators.compose([
+                    Validators.required
+                ])],
+                email:['', Validators.compose([
+                    Validators.required
+                    ])],
+                phone:['', Validators.compose([
+                    Validators.required
+                ])]
+            }),
+            address:fb.group({
+                street:['', Validators.compose([
+                    Validators.required
+                ])],
+                suite:['', Validators.compose([
+                    Validators.required
+                ])],
+                city:['', Validators.compose([
+                    Validators.required
+                ])],
+                zipcode:['', Validators.compose([
+                    Validators.required
+                ])]                      
+            })      
+
+        });
+    }
+
 }
