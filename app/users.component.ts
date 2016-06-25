716e9954422e72080722caadc5365f4a8f8cf28b
cristianmurillo87 <cam_725@hotmail.com>
2016-06-25 01:03:51 -0500

    Creado servicio para cargar usuarios dinamicamente y creado boton de "Agregar Usuario"

--- app/users.component.ts
+++ app/users.component.ts
@@ -1,8 +1,23 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+import {HTTP_PROVIDERS} from 'angular2/http';
+import {UsersService} from './users.service';
+
 
 @Component({
-    templateUrl : 'app/users.component.html'
+    templateUrl : 'app/users.component.html',
+    providers: [UsersService, HTTP_PROVIDERS]
 })
-export class UsersComponent{
-    
+export class UsersComponent implements OnInit{
+    users;
+
+    constructor( private _usersService: UsersService){
+
+    }
+
+    ngOnInit(){
+       this._usersService.getUsers()
+        .subscribe(users =>{
+            this.users = users;
+        });
+    }
 }
